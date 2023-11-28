0d9b9aed4191bee58e4be8af90c48bb6fd6be15a
r-ca <nem@nem.x0.to>
2023-11-28 18:13:46 +0900

    Logger作った

---
+++ I-504/common/logger.py
@@ -0,0 +1,27 @@
+# LOGGER
+
+is_debug = True
+
+class Logger:
+    def __init__(self, tag:str):
+        self.tag = tag
+
+    def debug(self, msg:str):
+        if is_debug:
+            print(f"[DEBUG] {self.tag}:\t {msg}")
+
+    def info(self, msg:str):
+        print(f"[INFO] {self.tag}:\t {msg}")
+
+    def warn(self, msg:str):
+        print(f"[WARN] {self.tag}:\t {msg}")
+
+    def error(self, msg:str):
+        print(f"[ERROR] {self.tag}:\t {msg}")
+
+    def fatal(self, msg:str):
+        print(f"[FATAL] {self.tag}:\t {msg}")
+
+
+
+
