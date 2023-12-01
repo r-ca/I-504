8d40f01786c32828c13f2491d51ce05bd41a0dba
r-ca <nem@nem.x0.to>
2023-12-01 18:27:55 +0900

    TwitterUsersモデル作った

--- I-504/db_model/source/twitter.py
+++ I-504/db_model/source/twitter.py
@@ -8,5 +8,17 @@ class TwitterTweets(Base):
     entry_id = Column(String(64), primary_key=True)
     created_at = Column(DateTime)
     full_text = Column(String(4096))
-    user_rest_id = Column(String(64), ForeignKey('source_twitter_users.rest_id'))
+    user_rest_id = Column(Integer(64), ForeignKey('source_twitter_users.user_rest_id'))
+
+class TwitterUsers(Base):
+    __tablename__ = 'source_twitter_users'
+
+    user_rest_id = Column(Integer(64), primary_key=True)
+    screen_name = Column(String(64))
+    name = Column(String(64))
+    description = Column(String(4096))
+    protected = Column(Boolean)
+    banner_url = Column(String(1024))
+    profile_image_url = Column(String(1024))
+
 
