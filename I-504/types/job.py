e97b0e111fdf2fe7539060b3e4ea9ec40a17fb6d
r-ca <nem@nem.x0.to>
2023-12-02 16:39:26 +0900

    ドキュメント

--- I-504/types/job.py
+++ I-504/types/job.py
@@ -6,19 +6,24 @@ class IJobReqBody(metaclass=ABCMeta): # interface
     pass # TODO: ファイル分割
 
 class JobReqBody_Control(IJobReqBody):
+    """Job Manager自体をコントロールするリクエスト"""
     def __init__(self, command: JobManagerControlCommand):
         self.command = command
 
 class JobReqBody_Register(IJobReqBody):
+    """Job Managerにジョブを登録するリクエスト"""
     pass # TODO
 
 class JobReqBody_Unregister(IJobReqBody):
+    """Job Managerからジョブを削除するリクエスト"""
     pass # TODO
 
 class JobReqBody_Update(IJobReqBody):
+    """Job Managerのジョブを更新するリクエスト"""
     pass # TODO
 
-class JmRequest:
+class JobManagerRequest:
+    """Job ManagerにPipeで送るリクエスト"""
     def __init__(self, job_req_type: JobReqType, job_req_body: IJobReqBody):
         self.job_req_type = job_req_type
         self.job_req_body = job_req_body
