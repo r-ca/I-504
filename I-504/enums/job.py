12c785664d5602a640a9467329218a1ecf3b49f5
r-ca <nem@nem.x0.to>
2023-12-02 17:55:08 +0900

    ジョブ登録用の型とかをつくった

--- I-504/enums/job.py
+++ I-504/enums/job.py
@@ -12,3 +12,21 @@ class JobManagerControlCommand(Enum):
     """ジョブマネージャーをコントロールするコマンドの一覧"""
     STOP = "stop"
     TEST = "test" # 分岐テストコマンド
+
+class JobPriority(Enum):
+    """ジョブの優先度"""
+    LOW = 0
+    NORMAL = 1
+    HIGH = 2
+    CRITICAL = 3
+
+class JobStatus(Enum):
+    """ジョブのステータス"""
+    SCHEDULED = "scheduled" # 登録済み
+    WAITING_DEPEND = "waiting_depend" # 依存ジョブ待ち
+    RUNNING = "running" # 実行中
+    FINISHED = "finished" # 完了
+    WAITING_RETRY = "waiting_retry" # リトライ待ち
+    FAILED = "failed" # 失敗
+    CANCELED = "canceled" # キャンセル済み
+    UNKNOWN = "unknown" # 不明
--- I-504/types/job.py
+++ I-504/types/job.py
@@ -1,9 +1,36 @@
 from ..enums.job import *
 
+import pickle
+
 from abc import ABCMeta
 
 class IJobReqBody(metaclass=ABCMeta): # interface
     pass # TODO: ファイル分割
+
+class JobDepend:
+    """依存ジョブと仲介の設定"""
+    def __init__(self, depend_job_id: str, require_mediator: bool = False, mediator_func = None):
+        self.depend_job_id = depend_job_id # 依存ジョブのID
+        self.require_mediator = require_mediator # 仲介が必要かどうか
+        self.mediator_func = mediator_func # 仲介関数
+
+class JobMeta:
+    """Jobのメタデータ"""
+    def __init__(self, job_name: str, job_desc: str, priority: JobPriority, has_depend_job:bool , job_depend: JobDepend = None):
+        self.job_name = job_name # ジョブ名
+        self.job_desc = job_desc # ジョブの説明
+        self.priority = priority # ジョブの優先度
+        self.has_depend_job = has_depend_job # 依存ジョブを持つかどうか
+        self.job_depend = job_depend # 依存ジョブと仲介の設定
+
+class Job:
+    """実際に記録されるJobの型"""
+    def __init__(self, job_id: str, job_meta: JobMeta, job_func, args: tuple = (), kwargs: dict = {}):
+        self.job_id = job_id # ジョブID
+        self.job_meta = job_meta # ジョブのメタデータ
+        self.job_func = pickle.dumps(job_func) # 実行する関数
+        self.args = args # 関数に渡す引数
+        self.kwargs = kwargs # 関数に渡すキーワード引数
 
 class JobReqBody_Control(IJobReqBody):
     """Job Manager自体をコントロールするリクエスト"""
@@ -12,7 +39,9 @@ class JobReqBody_Control(IJobReqBody):
 
 class JobReqBody_Register(IJobReqBody):
     """Job Managerにジョブを登録するリクエスト"""
-    pass # TODO
+    def __init__(self, job_id:str, job: Job):
+        self.job_id = job_id # ジョブID(UUIDv4)
+        self.job = job # ジョブの内容
 
 class JobReqBody_Unregister(IJobReqBody):
     """Job Managerからジョブを削除するリクエスト"""
