8e34f075d01e5d7ab63924a040879684ae7821a5
ろむねこ <nem@nem.x0.to>
2023-12-04 10:46:30 +0900

    WIP

---
+++ I-504/process/job_manager/utils/interval_executer.py
@@ -0,0 +1,7 @@
+
+class IntervalExecuter:
+    def __init__(self, interval: int, job_func):
+        self.interval = interval
+        self.job_func = job_func
+    def start():
+        """実行開始する"""
