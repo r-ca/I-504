512e2304678d7e7e96a922919f21948d73282a23
ろむねこ <nem@nem.x0.to>
2023-12-04 10:46:43 +0900

    Revert "WIP"
    This reverts commit 8e34f075d01e5d7ab63924a040879684ae7821a5.

--- I-504/process/job_manager/utils/interval_executer.py
+++
@@ -1,7 +0,0 @@
-
-class IntervalExecuter:
-    def __init__(self, interval: int, job_func):
-        self.interval = interval
-        self.job_func = job_func
-    def start():
-        """実行開始する"""
